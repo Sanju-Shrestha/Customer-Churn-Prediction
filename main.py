c023a1bd1f4dc9cc14d8d9e5557f0cd5bf0a418f
Sanju-Shrestha <sanjushresthanp@gmail.com>
2024-10-04 18:38:57 +0545

    04 Task Model Trainer LightGBM

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.customer_churn import logging
 from src.customer_churn.pipelines.pipe_01_data_ingestion import DataIngestionPipeline
 from src.customer_churn.pipelines.pipe_02_data_validation import DataValidationPipeline
 from src.customer_churn.pipelines.pipe_03_data_transformation import DataTransformationPipeline
+from src.customer_churn.pipelines.pipe_04_model_trainer import ModelTrainerPipeline
 
 ELEMENT_01_NAME = "DATA INGESTION ELEMENT"
 try:
@@ -28,6 +29,16 @@ try:
     logging.info(f"## =================== {ELEMENT_03_NAME} Started! ========================##")
     data_transformation_pipeline = DataTransformationPipeline()
     data_transformation_pipeline.run()
+    logging.info(f"## =============== {ELEMENT_03_NAME} Terminated Successfully!=================\n\nx************************x")
+except Exception as e:
+    logging.exception(e)
+    raise e
+
+ELEMENT_04_NAME = "MODEL TRAINER ELEMENT"
+try:
+    logging.info(f"## =================== {ELEMENT_03_NAME} Started! ========================##")
+    model_trainer_pipeline = ModelTrainerPipeline()
+    model_trainer_pipeline.run()
     logging.info(f"## =============== {ELEMENT_03_NAME} Terminated Successfully!=================\n\nx************************x")
 except Exception as e:
     logging.exception(e)
